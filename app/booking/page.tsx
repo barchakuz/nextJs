74c3eafc7024a4077998e9512720537c29eeb2f7
Barchakuz <behroze.barcha@gmail.com>
2024-05-09 00:24:38 +0500

    nav bar

--- app/booking/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ChaiPage() {
-  return (
-    <><h1>Chai Page</h1></>
-  )
-}
-
-export default ChaiPage
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className=" w-full mt-3 font-mono text-center">Nav Bar</h1>
+      
     </>
   );
 }
