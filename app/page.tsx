f879639df8ca8a4995031da0ea81a5e0062e98d8
Barchakuz <behroze.barcha@gmail.com>
2024-05-10 15:35:16 +0500

    whychoseus panel added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import FeaturedCourse from "./components/FeaturedCourse";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeaturedCourse />
+      <WhyChooseUs />
     </>
   );
 }
