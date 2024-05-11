7ca17a9786d7549996aa96f61a4b8c9d033b6c7b
Barchakuz <behroze.barcha@gmail.com>
2024-05-11 15:23:35 +0500

    background grid

--- app/components/MusicSchoolCards.tsx
+++ app/components/MusicSchoolCards.tsx
@@ -36,7 +36,7 @@ const musicSchoolTestimonials = [
 
 function MusicSchoolCards() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
         <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
         <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-6xl">
